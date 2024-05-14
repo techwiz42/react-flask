95bd0dbc61aa8b6e9eb6018456de2e9f6073699c
techwiz42 <thetechwizard42@gmail.com>
2024-05-14 23:19:40 +0000

    added rudimentary JWT auth

--- src/App.js
+++ src/App.js
@@ -1,46 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import Header from './components/Header'
+import useToken from './components/useToken'
+import './App.css'
 
-// HERE IS THE MAGIC
 function App() {
-  
-  const [currentTime, setCurrentTime] = useState(0)
-  const [theJoke, setTheJoke] = useState(0) 
-
-  useEffect(() => {
-    fetch("http://thewizard.online:5000/time")
-        .then(res => res.json()).then(data => 
-	      { setCurrentTime(data.time);
-    });
-  }, []);
-  useEffect(() => {
-    fetch("http://thewizard.online:5000/joke")
-	.then(res => res.json()).then(data =>
-	      { setTheJoke(data);
-    });
-  }, []);
+  const { token, removeToken, setToken } = useToken();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-	<p>The current time is {currentTime} - time for a random joke.</p>
-	<p>{theJoke.setup} {theJoke.punchline}</p>  
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header token={removeToken}/>
+        {!token && token!=="" &&token!== undefined?  
+        <Login setToken={setToken} />
+        :(
+          <>
+            <Routes>
+              <Route exact path="/profile" element={<Profile token={token} setToken={setToken}/>}></Route>
+            </Routes>
+          </>
+        )}
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
